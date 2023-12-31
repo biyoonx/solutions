b3c14f89267ac8b4eef80cc03350f8902faca640
biyoonx <ok2geoya@gmail.com>
2023-12-31 19:19:18 +0900

    first commit

---
+++ a_balanced_parenthesis.py
@@ -0,0 +1,26 @@
+parenthesis_set = [ ['{', '}' ], [ '[', ']' ], [ '(', ')' ], [ '<', '>' ] ]
+
+def isOpen(open_char):
+		for paren in parenthesis_set:
+				if (open_char == paren[0]):
+						return True
+		return False
+
+def isPaired(open_char, closed_char):
+		for paren in parenthesis_set:
+				if ((open_char == paren[0]) and (closed_char == paren[1])):
+						return True
+		return False
+
+def isBalanced(input_paren):
+		paren_stack = []
+		for paren in input_paren:
+				if (isOpen(paren)):
+						paren_stack.append(paren)
+				else:
+						isPaired(paren_stack.pop(), paren)
+		return not paren_stack
+
+input_arr = list(input('input > '))
+result = isBalanced(input_arr)
+print('result >', result)
