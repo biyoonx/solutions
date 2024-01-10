999bc34be61eea72a804e37e80ed66493d794386
biyoonx <ok2geoya@gmail.com>
2024-01-10 22:16:38 +0900

    add some comment

--- c_cycles_in_a_linked_list.py
+++ c_cycles_in_a_linked_list.py
@@ -26,7 +26,7 @@ class LinkedList:
 				self.head = self.head.next
 				return data_out
     
-    # 여기서부터
+    # 여기서부터, 어떻게 해야 index번째에 있는 데이터를 찾아서 삽입/삭제할 수 있을까?
 		def insert(self, data, index):
 				data_in = self.Node(data)
 				range(index)
